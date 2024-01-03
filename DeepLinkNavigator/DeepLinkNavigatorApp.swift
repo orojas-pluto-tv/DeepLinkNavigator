dc242a732a3d1e61b60b1f395dd5746def0c77b8
Octavio Rojas <octavio.rojas@pluto.tv>
2024-01-03 15:47:14 -0600

    Initial Commit

---
+++ DeepLinkNavigator/DeepLinkNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkNavigatorApp.swift
+//  DeepLinkNavigator
+//
+//  Created by Octavio Rojas on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
